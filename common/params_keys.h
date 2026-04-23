aee2fdbec1e8cbfb0a32949bde8de18b5ff57714
Thiago Andrade <thiagogpa@gmail.com>
2026-04-23 00:24:22 -0400

    feat: add EnableDriverMonitoring param

--- common/params_keys.h
+++ common/params_keys.h
@@ -9,6 +9,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"AccessToken", {CLEAR_ON_MANAGER_START | DONT_LOG, STRING}},
     {"AdbEnabled", {PERSISTENT | BACKUP, BOOL}},
     {"AlwaysOnDM", {PERSISTENT | BACKUP, BOOL}},
+    {"EnableDriverMonitoring", {PERSISTENT | BACKUP, BOOL, "1"}},
     {"ApiCache_Device", {PERSISTENT, STRING}},
     {"ApiCache_FirehoseStats", {PERSISTENT, JSON}},
     {"AssistNowToken", {PERSISTENT, STRING}},
